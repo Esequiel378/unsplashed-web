423163013f79c354f7290c6f12dfb489f4e969e0
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-01-23 15:06:21 -0300

    :zap: Add and configure react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { AppProps } from "next/app";
 import theme from "styles/theme";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   useEffect(() => {
@@ -13,12 +15,20 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     }
   }, []);
 
+  const [queryClient] = useState(() => new QueryClient());
+  const dehydratedState = pageProps;
+
   return (
     <>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-          <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={dehydratedState}>
+            <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </Hydrate>
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
