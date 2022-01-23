b4da527c00e727d4c4834638f1604e17fab9065b
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-01-23 16:58:13 -0300

    :bug: :fire: Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import { Button } from '@material-ui/core';
 import type { NextPage } from 'next';
 
-t = { a: 1 };
-
 const Home: NextPage = () => {
   return (
     <Button color="primary" variant="contained">
