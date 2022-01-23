ee8e60737e93ff9dc8d03490f6d6a2c33abdfd93
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-01-23 16:13:17 -0300

    ⚡ Feature - Add unsplashed service (#3)
    * :zap: Add axios http client
    
    * :zap: Add response types
    
    * :zap: Add photosList queries
    
    * :zap: Add searchPhotos queries

---
+++ services/unsplashed/photos/index.ts
@@ -0,0 +1,1 @@
+export { usePhotosList, photosPrefetchList } from "./queries";
---
+++ services/unsplashed/search/queries.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "react-query";
+import http from "services/unsplashed/http-common";
+import { PaginatedPhoto } from "services/unsplashed/types";
+
+const searchPhotos = async (query: string): Promise<PaginatedPhoto> => {
+  const { data } = await http.get<PaginatedPhoto>("/search/photos", {
+    params: {
+      query,
+    },
+  });
+
+  return data;
+};
+
+interface SearchPhotosProps {
+  query: string;
+}
+
+export const useSearchPhotos = ({ query: queryString }: SearchPhotosProps) => {
+  const query = useQuery<PaginatedPhoto>(["search", "photos", queryString], () =>
+    searchPhotos(queryString)
+  );
+
+  return query;
+};
