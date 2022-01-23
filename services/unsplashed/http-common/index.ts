5c72a770c06b6d2340550e0e7daf39a9abefc975
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-01-23 15:47:23 -0300

    :zap: Add axios http client

---
+++ services/unsplashed/http-common/index.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const URL = process.env.UNSPLASHED_URL ?? "http://localhost:4000/api/unsplash";
+
+const http = axios.create({
+  baseURL: URL,
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
+export default http;
