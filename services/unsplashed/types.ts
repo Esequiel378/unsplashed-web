2b48f628b3146c161882cb66c102ac66a3e3c407
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-01-23 16:09:03 -0300

    :zap: Add response types

---
+++ services/unsplashed/types.ts
@@ -0,0 +1,93 @@
+interface Urls {
+  raw: string;
+  full: string;
+  regular: string;
+  small: string;
+  thumb: string;
+}
+
+interface Links {
+  self: string;
+  html: string;
+  download: string;
+  download_location: string;
+}
+
+interface SponsorLinks {
+  self: string;
+  html: string;
+  photos: string;
+  likes: string;
+  portfolio: string;
+  following: string;
+  followers: string;
+}
+
+interface ProfileImage {
+  small: string;
+  medium: string;
+  large: string;
+}
+
+interface Social {
+  instagram_username: string;
+  portfolio_url: string;
+  twitter_username: string;
+  paypal_email?: any;
+}
+
+interface Sponsor {
+  id: string;
+  updated_at: Date;
+  username: string;
+  name: string;
+  first_name: string;
+  last_name?: any;
+  twitter_username: string;
+  portfolio_url: string;
+  bio: string;
+  location?: any;
+  links: SponsorLinks;
+  profile_image: ProfileImage;
+  instagram_username: string;
+  total_collections: number;
+  total_likes: number;
+  total_photos: number;
+  accepted_tos: boolean;
+  for_hire: boolean;
+  social: Social;
+}
+
+interface Sponsorship {
+  impression_urls: string[];
+  tagline: string;
+  tagline_url: string;
+  sponsor: Sponsor;
+}
+
+export interface Photo {
+  id: string;
+  created_at: Date;
+  updated_at: Date;
+  promoted_at: Date;
+  width: number;
+  height: number;
+  color: string;
+  blur_hash: string;
+  description: string;
+  alt_description: string;
+  urls: Urls;
+  links: Links;
+  categories: any[];
+  likes: number;
+  liked_by_user: boolean;
+  current_user_collections: any[];
+  sponsorship: Sponsorship;
+
+}
+
+export interface PaginatedPhoto {
+    total: number;
+    total_pages: number;
+    results: Photo[];
+};
