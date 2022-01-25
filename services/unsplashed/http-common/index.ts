e2421fafc7c590c2974ed2c1ace80c926741edd5
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-01-25 15:12:38 -0300

    Debug urls

--- services/unsplashed/http-common/index.ts
+++ services/unsplashed/http-common/index.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 const URL = process.env.UNSPLASHED_URL ?? 'http://localhost/api';
 const SERVICE_URL = `${URL}/unsplash`;
 
+console.log(URL);
+console.log(SERVICE_URL);
+
 const http = axios.create({
   baseURL: SERVICE_URL,
   headers: {
